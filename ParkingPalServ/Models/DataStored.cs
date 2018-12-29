00a4adc0efe7c0982de4b30449e164275ebce52a
Zach Wissing <zwissing@imagesoftinc.com>
2018-12-29 02:01:11 -0500

    fixed for azure

---
+++ ParkingPalServ/Models/DataStored.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingPalServ.Models
+{
+    public class DataStored
+    {
+        public string Total { get; set; }
+        public string Open { get; set; }
+        public string OpenH { get; set; }
+        public string Occupied { get; set; }
+        public string OccupiedH { get; set; }
+    }
+}
